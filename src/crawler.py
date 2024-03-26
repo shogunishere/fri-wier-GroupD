920af51031afd84a62dd521059ccc52fea1d7c73
Kurtaga <ak84795@student.uni-lj.si>
2024-03-26 21:40:53 +0100

    Unique page url fix

--- src/crawler.py
+++ src/crawler.py
@@ -5,7 +5,6 @@ from collections import deque
 from driver import get_driver
 from database import get_site, get_page, insert_site, insert_page, insert_link, insert_image
 from utils import get_domain, get_base_url, get_url_path, get_http_headers, get_urls, fetch_images, fetch_robots_txt, fetch_sitemap
-from models import PageType
 
 def crawl(seed_urls, depth):
     driver = get_driver()
@@ -25,9 +24,7 @@ def crawl(seed_urls, depth):
         driver.get(url)
         html = driver.page_source
 
-        # TODO: check hash
-        
-        # Get headers with a new request
+        # Get http headers with a new request
         status_code, page_type_code = get_http_headers(url)
 
         # Process site
@@ -37,27 +34,27 @@ def crawl(seed_urls, depth):
         # Process page
         path_url = get_url_path(url)
         page_id = process_page(path_url, site_id, status_code, page_type_code, html, accessed_time)
+        
+        # Continue only if page is not yet saved in the database
+        if page_id is not None: 
+            # Link if not the seed
+            if (from_page_id): insert_link(from_page_id, page_id)
 
-        # Link if not the seed
-        if (from_page_id): insert_link(from_page_id, page_id)
+            # Process images
+            imgs = fetch_images(html, base_url)
+            for filename, content_type, data in imgs:
+                process_image(page_id, filename, content_type, data, accessed_time)
 
-        # Process images
-        imgs = fetch_images(html, base_url)
-        for filename, content_type, data in imgs:
-            process_image(page_id, filename, content_type, data, accessed_time)
-
-        # Populate queue with newly found URLs
-        new_urls = get_urls(html, url)
-        for new_url in new_urls:
-            queue.append((page_id, new_url, current_depth + 1))
+            # Add to queue newly found URLs
+            new_urls = get_urls(html, url)
+            for new_url in new_urls:
+                queue.append((page_id, new_url, current_depth + 1))
 
     driver.quit()
 
 def process_site(base_url):
     robots_txt = fetch_robots_txt(base_url)
     sitemap = fetch_sitemap(base_url)
-
-    # TODO: Check if site exists with get_site
 
     domain = get_domain(base_url)
 
@@ -73,17 +70,18 @@ def process_site(base_url):
 
 def process_page(url, site_id, status_code, page_type_code, html, accessed_time):
 
+    # TODO: check hash
+
     page_id = get_page(url)
 
     if (page_id is None):
         page_id = insert_page(site_id, page_type_code, url, html, status_code, accessed_time)
         print(f"New page [id: {page_id}]")
+        return page_id
     else:
-        page_id = insert_page(site_id, PageType.DUPLICATE, url, None, status_code, accessed_time)
         print(f"Existing page [id: {page_id}]")
-
-    return page_id
+        return None
 
 def process_image(page_id, filename, content_type, data, accessed_time):
-    insert_image(page_id, filename, content_type, data, accessed_time)
+    image_id = insert_image(page_id, filename, content_type, data, accessed_time)
     print(f"New image [filename: {filename}]")

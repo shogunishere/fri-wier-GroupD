4de398f855f9c8473497f52d1e6bd412f5f2f461
Kurtaga <ak84795@student.uni-lj.si>
2024-04-03 14:57:23 +0200

    Fixed driver instance for each thread

--- src/driver.py
+++ src/driver.py
@@ -8,7 +8,7 @@ driver_instance = None
 def setup_driver():
     global driver_instance
     chrome_options = ChromeOptions()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
 
     service = ChromeService(executable_path=DRIVER_LOCATION)
     driver_instance = webdriver.Chrome(service=service, options=chrome_options)
@@ -18,4 +18,11 @@ def get_driver():
     if driver_instance is None:
         setup_driver()
     return driver_instance
+    
+def get_driver():
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless")
+    service = ChromeService(executable_path=DRIVER_LOCATION)
+    return webdriver.Chrome(service=service, options=chrome_options)
+
 
